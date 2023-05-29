75b36ee780f573caa753a225e7890d79931ff835
aafrinsayani22 <aafrinsayani22@gmail.com>
2023-05-29 15:59:40 -0400

    Add Files

---
+++ ColorMaker/ColorMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMakerApp.swift
+//  ColorMaker
+//
+//  Created by Aafrin Sayani on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct ColorMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
